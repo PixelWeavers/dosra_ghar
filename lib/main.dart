4f03b0cfb1eb4afc289274bd55b33815991abb06
Raazi Faisal <raazi.faisal@gmail.com>
2024-03-31 12:09:09 +0530

    added files to git ignored

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:dosra_ghar/views/profile.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load(fileName: ".env");
   runApp(
     const MyApp(),
   );
