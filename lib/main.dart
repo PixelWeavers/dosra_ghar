864ac6ec0f6d357cfd80eaa217c4e48bdd24ee16
Raazi Faisal <raazi.faisal@gmail.com>
2024-04-06 12:16:24 +0530

    removed-env

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,13 @@ import 'package:dosra_ghar/views/menu.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:dosra_ghar/views/profile.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await dotenv.load(fileName: ".env");
+
   runApp(
     const MyApp(),
   );
