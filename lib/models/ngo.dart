ccf5934d082b619f0b75207da22b5d36f2115a45
teotia-codes <priyanshu.teotia50@gmail.com>
2024-04-09 11:36:28 +0530

    volunteer screen ui done

---
+++ lib/models/ngo.dart
@@ -0,0 +1,11 @@
+class Ngo {
+  final String logoUrl;
+  final String name;
+  final String description;
+
+  const Ngo({
+    required this.logoUrl,
+    required this.name,
+    required this.description,
+  });
+}
